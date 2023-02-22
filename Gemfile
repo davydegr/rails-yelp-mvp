792da15b210ada0c6917eddd7868879c8c217656
Davy De Groote <davy.degr@gmail.com>
2023-02-22 14:37:13 +0100

    Finished MVP

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "3.1.2"
 
 # Custom gems
 gem "simple_form", github: "heartcombo/simple_form"
+gem "faker", github: "faker-ruby/faker"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.2"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Deleting all existing restaurants ...'
+puts 'Automatically deleting all reviews because of dependent:destroy'
+
+Restaurant.destroy_all
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Games::Pokemon.move,
+    address: Faker::Games::Pokemon.location,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+
+
+  restaurant.save
+  puts "Generated #{restaurant.name} with ID = #{restaurant.id}"
+
+  puts "Generating 3 reviews for #{restaurant.name}"
+  3.times do
+    review = Review.new(
+      rating: rand(0..5),
+      content: Faker::Quotes::Shakespeare.hamlet_quote
+    )
+
+    review.restaurant = restaurant
+
+    review.save
+
+    puts '--- Review saved'
+  end
+end
